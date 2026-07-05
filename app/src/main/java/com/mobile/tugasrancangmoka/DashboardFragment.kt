744347db7505daa9240d1739a58e6e21fd5148ee
Anisa Nedeland <149438742+anisanedeland23@users.noreply.github.com>
2026-07-05 15:49:16 +0700

    wip: perubahan kecil dashboard

--- app/src/main/java/com/mobile/tugasrancangmoka/DashboardFragment.kt
+++ app/src/main/java/com/mobile/tugasrancangmoka/DashboardFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import com.mobile.tugasrancangmoka.R
 import com.mobile.tugasrancangmoka.adapter.StockAdapter
 import com.mobile.tugasrancangmoka.model.StockItem
 import com.mobile.tugasrancangmoka.model.StockStatus
