eeb9aebf343a372f956725ffd2b9d336ee9cd85b
Billal Cahya Nugraha <billalcahya@gmail.com>
2026-07-10 18:48:06 +0700

    Remove js file

--- routes/dashboardRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const dashboardController = require('../controllers/dashboardController');
-const authMiddleware = require('../middleware/authMiddleware');
-
-router.get('/', authMiddleware, dashboardController.getDashboardData);
-
-module.exports = router;
--- routes/inventoryRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const inventoryController = require('../controllers/inventoryController');
-const authMiddleware = require('../middleware/authMiddleware');
-
-router.get('/', authMiddleware, inventoryController.getInventory);
-router.put('/:product_id', authMiddleware, inventoryController.updateStock);
-
-module.exports = router;
